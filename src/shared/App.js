5b67e8e6105e6c0e4baddb4f89bf6985d38ee066
Jinnycorn <wldms949494@gmail.com>
2021-04-12 14:00:22 +0900

    다시수정

---
+++ src/shared/App.js
@@ -0,0 +1,40 @@
+
+import React, {useState} from 'react';
+
+import styled from 'styled-components';
+
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import { ConnectedRouter } from "connected-react-router";
+import { history } from "../redux/configureStore";
+import { useDispatch, useSelector } from "react-redux";
+import { actionCreators as userActions } from "../redux/modules/user"
+
+
+
+function App() {
+
+  const dispatch = useDispatch()
+
+ 
+    return (
+      <ReactContainer>
+        {/* <Header/> */}
+        <ConnectedRouter history={history}>
+
+        {/* <Switch>
+          <Route path="/" exact component={PostList}/>
+          <Route path="/upload" exact component={PostWrite}/>
+          <Route path="/upload/:id" exact component={PostWrite}/> */}
+          {/* NotFound 페이지 */}
+          {/* <Route exact component={NotFound}/>
+        </Switch> */}
+
+
+        </ConnectedRouter>
+      </ReactContainer>
+    );
+  
+ 
+}
+
+export default App;
