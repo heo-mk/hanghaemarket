e76afdf273136445f33bda8b25b052360eccc377
Jinnycorn <wldms949494@gmail.com>
2021-04-13 20:24:13 +0900

    detail수정

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -2,6 +2,11 @@ import React,{useState} from 'react';
 import styled from 'styled-components';
 import img from "../shared/watch6.jpg";
 
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+
+
+
 const Detail =(props)=>{
 
 
@@ -13,18 +18,24 @@ const Detail =(props)=>{
                 <ProductContentsBox>
                     <h3>애플워치6 스테인리스 싸게 팝니다.</h3>
                     <PriceBox><p><span>110,000</span> 원</p></PriceBox>
+                   
                     <p>상품상태: 사용감 있음</p>
                     <p>배송비: 배송비 별도</p>
                     <p>거래지역: 분당, 판교, 강남</p>    
                 </ProductContentsBox>            
                 <ButtonBox>
-                    <LikeButton>찜</LikeButton>
+                  
+                    <LikeButton>
+                    <FavoriteBorderIcon fontSize="small"/>
+                      찜
+                    </LikeButton>
+                     
                     <ChatButton>채팅하기</ChatButton>
                 </ButtonBox>            
             </InfoBox>
         </SellInfo>
 
-        
+        <WrapInfo>
             <SelectInfoBar>
                 <ProductInfoButton><h3>상품정보</h3></ProductInfoButton>
                 <StoreInfoButton><h3>상점정보</h3></StoreInfoButton>
@@ -33,37 +44,18 @@ const Detail =(props)=>{
             <WrapSelectInfo>
                 <ProductInfo>
                     <div><h3>상품정보</h3></div>
-                    <div>사용감 있는 상품입니다. 반품, 환불은 불가해요.</div>
+                    <div>사용감 있는 상품입니다. 반품, 환불은 불가합니다.</div>
+                    
+                    <div>상품문의</div>
+                    <div><input text/></div>
+                    <button>등록</button>
+
                 </ProductInfo>
                 <StoreInfo>
 
                 </StoreInfo>
             </WrapSelectInfo>
-{/*         
-<Card>
-  <Card.Header>
-    <Nav variant="tabs" defaultActiveKey="#first">
-      <Nav.Item>
-        <Nav.Link href="#first">Active</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link href="#link">Link</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link href="#disabled" disabled>
-          Disabled
-        </Nav.Link>
-      </Nav.Item>
-    </Nav>
-  </Card.Header>
-  <Card.Body>
-    <Card.Title>Special title treatment</Card.Title>
-    <Card.Text>
-      With supporting text below as a natural lead-in to additional content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card> */}
+        </WrapInfo>
 
     </React.Fragment>
         
@@ -73,13 +65,14 @@ const Detail =(props)=>{
 const SellInfo=styled.div`
 display: flex;
 padding: 30px 0px;
-padding-top: 30px;
-padding-bottom: 30px;
 justify-content:center;
+padding-top: 120px;
+padding-bottom: 20px;
 
 `;
 
 const ImgBox=styled.div`
+margin-top: 15px;
 margin-right: 150px;
 flex-shrink: 0;
 `;
@@ -112,11 +105,14 @@ const PriceBox=styled.div`
 padding-bottom: 3px;
 border-bottom: 1px solid rgb(238, 238, 238);
 `;
+
+
 const ButtonBox=styled.div`
 display: flex;
 -webkit-box-pack: justify;
 justify-content: space-between;
 `;
+
 
 const LikeButton=styled.button`
 margin-right:30px;
@@ -125,8 +121,13 @@ background-color:#6fa1f6;
 border-radius:30px;
 margin:8px;
 border:1px solid #6fa1f6;
-width:10vw;
+width:15vw;
+font-size:15px;
+color:#fff;
 `;
+
+
+
 
 const ChatButton=styled.button`
 
@@ -135,10 +136,14 @@ background-color:#6fa1f6;
 border-radius:30px;
 margin:8px;
 border:1px solid #6fa1f6;
-width:10vw;
+width:15vw;
+font-size:15px;
+color:#fff;
 `;
 
-
+const WrapInfo=styled.div`
+justify-content:center;
+`;
 const SelectInfoBar=styled.div`
 display: flex;
 height: 70px;
@@ -207,7 +212,7 @@ height: calc(100% - 50px);
 `;
 
 const ProductInfo=styled.div`
-
+margin-left: 100px;
 `;
 const StoreInfo=styled.div`
 
