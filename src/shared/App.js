bcd9ee7e3f8de364ca3cce5d40b846f0b425441a
Jinnycorn <wldms949494@gmail.com>
2021-04-13 10:12:30 +0900

    0413수정

--- src/shared/App.js
+++ src/shared/App.js
@@ -9,7 +9,8 @@ import { history } from "../redux/configureStore";
 import { useDispatch, useSelector } from "react-redux";
 // import { actionCreators as userActions } from "../redux/modules/user"
 
-
+import Detail from '../pages/Detail';
+import Login from "../pages/Login";
 
 function App() {
 
@@ -21,15 +22,17 @@ function App() {
         {/* <Header/> */}
         <ConnectedRouter history={history}>
 
-        {/* <Switch>
-          <Route path="/" exact component={PostList}/>
-          <Route path="/upload" exact component={PostWrite}/>
-          <Route path="/upload/:id" exact component={PostWrite}/> */}
+        <Switch> 
+            <Route path="/login" exact component={Login}/>
+        </Switch> 
+         
+          {/* {/* <Route path="/upload" exact component={PostWrite}/>
+          <Route path="/upload/:id" exact component={PostWrite}/> */} 
           {/* NotFound 페이지 */}
-          {/* <Route exact component={NotFound}/>
-        </Switch> */}
+          {/* <Route exact component={NotFound}/> */}
+       
 
-
+        <Detail/>
         </ConnectedRouter>
       </ReactContainer>
     );
