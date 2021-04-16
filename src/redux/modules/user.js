ed3d1f2e70232c1aa1d147c627aad0a7698005be
heo_minkyu <79818840+heo-mk@users.noreply.github.com>
2021-04-16 10:37:14 +0900

    4/16까지 로그인/회원가입문제

--- src/redux/modules/user.js
+++ src/redux/modules/user.js
@@ -22,7 +22,6 @@ const initialState = {
     street: ""
   },
   is_login: false,
-  // token: false,
 }
 
 // middleware : 백엔드 서버와 비동기 통신을 하기 위한 미들웨어
@@ -36,126 +35,148 @@ const initialState = {
 // 먼저 처리 해서 컴포넌트로 데이터를 가져온다.
 // 위 지식의 출처는 리액트 기본주차 5주차 강의 자료
 
-// 회원가입 미들웨어
+// 회원가입 요청(request, 클라이언트 -> 서버) 미들웨어
 const signupAPI = (email, password, username, city, street) => {
   return function (dispatch, getState, {history}) {
     // if(!profile_url){
     //   profile_url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBjZn8mOw7F4rtWWKbEIIHOr_w_GAeHiXPgA&usqp=CAU"
     // }
+    console.log(email, password, username, city, street);
+    
+    const API = "http://dmsql5303.shop/signups";
+    console.log(API);
 
-    // axios.post("http://dmsql5303.shop/signups", {
-    //   // 클라이언트에서 서버로 request(요청)하며 보내주는 데이터
-    //   // 회원가입창에서 클라이언트가 입력하는 데이터
-    //   email: email,           
-    //   password: password,  // 숫자, 영어 대문자, 소문자, 특수기호, 8-20자
-    //   username: username,  // id개념, 한글이 아니라 영어로 보내기, 영어+숫자, 4-12글자
-    //   city: city,           
-    //   street: street,
-    // })
-    const API = 'http://dmsql5303.shop/signups';
-      fetch(API, {
-        method: 'POST',
+    axios.post(API,
+      // 클라이언트에서 서버로 request(요청)하며 보내주는 데이터
+      // 회원가입창에서 클라이언트가 입력하는 데이터
+      {
+        email: email,           
+        password: password,  // 숫자, 영어 대문자, 소문자, 특수기호, 8-20자
+        username: username,  // id개념, 한글이 아니라 영어로 보내기, 영어+숫자, 4-12글자
+        city: city,           
+        street: street,
+      },
+      {
         headers: {
           'Content-Type': 'application/json',
+          // 'Accept': 'application/json',
         },
-        body: JSON.stringify({
-          email: email,
-          username: username,
-          password: password,
-          city: city,
-          street: street,
       })
-    })
-      // 그러면 서버에서 클라이언트로 response(응답)으로 true가 온다.
-    .then((response) => {
-      console.log(response.data)  // 서버에서 response로 {ok: true} 아니면 {ok: false}가  온다
-      history.replace('/logins')
-    }).catch((error) => {
-      window.alert('회원가입이 정상적으로 이뤄지지 않았습니다.')
-    })
-  }
-}
-
-// 로그인 요청(request, 클라이언트->서버) 미들웨어
-const loginAPI = (username, password) => {
-  return function (dispatch, getState) {
-  //   axios.post("http://dmsql5303.shop/login", {
-  //     // 클라이언트에서 서버로 request(요청)하며 보내주는 데이터
-  //     // 로그인창에서 클라이언트가 입력하는 데이터
-  //     username: username,
-  //     password: password,
-  // });
-
-    const API = 'http://dmsql5303.shop/login';
-      fetch(API, {
-        method: 'POST',
-        headers: {
-        'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          username: username,
-          password: password,
-      })
-    }) 
-    // 그러면 서버에서 클라이언트로 response(응답)으로 token(JWT)이 온다.
-    // .then((response) => {
-      // console.log(response);
-      // if(response.status === 200){
-      //   let token = response.headers.get("Authorization");
-      //   let userInfo = response.json();
-      //   console.log(userInfo);
-      //   // userInfo = JSON.parse('userInfo');
-      //   // console.log(userInfo);
-      //   // userInfo = JSON.parse(userInfo);
-      //   // console.log(userInfo);
-      //   // console.log(_token)
-      //   localStorage.setItem("token", token)
-      //   dispatch(setUser(token))
-      //   history.replace('/')
-      // }
-      .then((response) => response.json())  // 서버쪽에서 응답값(response)로 주는 것은 id와 name
+      // 그러면 서버에서 클라이언트로 response(응답)으로 
+      // {ok: true} 아니면 {ok: false}가 온다.
+      // .then((response) => {
+      //   console.log(response); // response.data로 해야?
+      // })
       .then((result) => {
         console.log(result);
-        let userInfo = result;
-        let token = result.headers.get('Authorization');  // console에서 여기가 에러 난다고 함
-        console.log(userInfo.id);
-        console.log(token)
-        // userInfo = JSON.parse(userInfo);
-        // userInfo.id = decodeURI(atob(userInfo.id));
-        // userInfo.name = decodeURI(atob(userInfo.name));
-        localStorage.setItem('token', token);
-        localStorage.setItem('userInfo', JSON.stringify(userInfo));
-        // dispatch(setUser({
-        //   uid: userInfo.id,
-        //   username: userInfo.name,
-        // }))
-        // history.push('/');
-      }).catch((error) => {
-        window.alert('로그인에 실패했습니다')
-        console.log(error)
+        console.log("singupDB!");
+        window.alert('회원가입이 되었습니다! 로그인 해주세요.');
+        history.replace('/login');
       })
-    }
+      .catch((error) => {
+        window.alert('회원가입이 정상적으로 되지 않았습니다.');
+        console.log(error);
+      })
+    };
+  };
+
+// 로그인 요청(request, 클라이언트 -> 서버) 미들웨어
+const loginAPI = (username, password) => {
+  return function (dispatch, getState, {history}) {
+
+    console.log(username, password);
+    const API = 'http://dmsql5303.shop/login';
+    axios.post(API, {
+      // 클라이언트에서 서버로 request(요청)하며 보내주는 데이터
+      // 로그인창에서 클라이언트가 입력하는 데이터
+      "username": username,
+      "password": password,
+    },
+    {
+      headers : {
+        "Content-Type": "application/json;charset=UTF-8",
+        // 'Content-Type' : 'application/json', //클라이언트가 서버한테 요청하는(원하는) 타입
+        'Accept' : 'application/json', //현재 서버한테 보내는 데이터 타입
+        // 'Access-Control-Allow-Origin' : '*',
+      },
+    })
+      // 그러면 서버에서 클라이언트로 response(응답)으로 
+      // header에선 token(JWT)이 오고,
+      // body에선 id, name이 온다.
+    .then(function (response) {
+      console.log(response);
+      console.log(response.data);
+      if (response.headers.token) {
+        console.log(response.data.token);
+        localStorage.setItem('is_token', response.data.token);
+        localStorage.setItem('uid', response.body.id);
+        localStorage.setItem('username', response.data.name);
+        // setUser를 발동시켜서
+        // 리덕스의 is_login 값을 true로 변경한다.
+        dispatch(
+          setUser(
+            {
+              uid: response.data.id,   
+              username: response.data.name,
+            }
+          )
+        )
+        window.alert('로그인 되었습니다!');
+        history.replace('/');
+      }
+      }).catch((error) => {
+        console.log(error);
+        window.alert('로그인 실패!')
+      });  
+    };
   }
+    
+// 로그인 상태 유지 체크 
+const loginCheckStore = () => {
+  return function (dispatch, getState, { history }) {
+    // 로컬스토리지의 토큰 가져오기
+    const token = localStorage.getItem('is_token');
+    const uid = localStorage.getItem('uid');
+    const username = localStorage.getItem('username');
+    // 토큰 없으면 재로그인
+    // 있다면 로그인 유지
+      if(!token) {
+        window.alert('재로그인이 필요합니다.')
+        return;
+      }else{
+        dispatch(setUser({
+          uid: uid,
+          uername: username,
+        })
+      )
+    }
+  };
+};
+
 
 const isLogin = () => {
   return function (dispatch, getState, { history }) {
-    const token = localStorage.getItem('token');
-    const userInfo = JSON.parse(localStorage.getItem('userInfo'));
+    const token = localStorage.getItem('is_token');
+    const uid = localStorage.getItem('uid');
+    const username = localStorage.getItem('username');
 
-    if (!token || !userInfo) {
+    if (!token) {
       return false;
     }
     dispatch(setUser({
-      uid: userInfo.id,
-      username: userInfo.name,
+      uid: uid,
+      username: username,
     }));
   }
 }
 
 // 로그아웃 : token 삭제만 하면 되므로 서버와 통신 불필요
+// 로컬스토리지의 토큰을 없앤다.
 const logout = () => {
   return function (dispatch, getstate) {
-    localStorage.removeItem('token');
+    localStorage.removeItem('is_token');
+    localStorage.removeItem("uid");
+    localStorage.removeItem("username")
     dispatch(logOut());
     history.replace('/');
   }
@@ -181,6 +202,7 @@ const actionCreators = {
   loginAPI,
   logout,
   isLogin,
+  loginCheckStore
 };
 
 export { actionCreators };
